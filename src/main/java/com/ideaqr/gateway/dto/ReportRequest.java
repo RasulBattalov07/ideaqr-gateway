a417ee25692beff4cb5b570c4747533a194b4b76
RasulBattalov07 <251731@astanait.edu.kz>
2026-06-16 23:00:18 +0500

    Update ReportRequest.java

--- src/main/java/com/ideaqr/gateway/dto/ReportRequest.java
+++ src/main/java/com/ideaqr/gateway/dto/ReportRequest.java
@@ -1,15 +1,9 @@
 package com.ideaqr.gateway.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-/** Payload for reporting an issue against an infrastructure / eco object. */
 @Data
 public class ReportRequest {
-
-    @NotBlank(message = "Не указан идентификатор объекта")
     private String objectUid;
-
-    /** Free-text description of the problem (Russian, from the citizen). */
     private String message;
 }
