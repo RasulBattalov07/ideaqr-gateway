706669eacd8ee28ac23cce925a8fc310e212f96c
RasulBattalov07 <251731@astanait.edu.kz>
2026-06-16 21:22:50 +0500

    Create QrService.java

---
+++ src/main/java/com/ideaqr/gateway/service/QrService.java
@@ -0,0 +1,9 @@
+package com.ideaqr.gateway.service;
+
+import com.ideaqr.gateway.dto.QrCreationRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QrService {
+    // Базовый сервис для генерации QR
+}
