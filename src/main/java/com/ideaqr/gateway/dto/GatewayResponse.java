3f43df2db19a455e821507731b3670623b60f04f
RasulBattalov07 <251731@astanait.edu.kz>
2026-06-16 23:00:35 +0500

    Update GatewayResponse.java

--- src/main/java/com/ideaqr/gateway/dto/GatewayResponse.java
+++ src/main/java/com/ideaqr/gateway/dto/GatewayResponse.java
@@ -1,10 +1,21 @@
 package com.ideaqr.gateway.dto;
 
 import lombok.Data;
+import java.util.UUID;
 
 @Data
 public class GatewayResponse {
-    private String status;
-    private String message;
-    private Object dataPayload;
+    private String outcome;
+    private String reason;
+    private String riskLevel;
+    private String category;
+    private String objectUid;
+    private Object data;
+
+    // Поля для анимации цепочки управления
+    private UUID identityUid;
+    private UUID requestUid;
+    private UUID decisionUid;
+    private UUID interactionUid;
+    private UUID historyUid;
 }
