03bcd2601625c39c829e74302f4770fb2d3c5563
RasulBattalov07 <251731@astanait.edu.kz>
2026-06-17 19:32:27 +0500

    Delete src/main directory

--- src/main/java/com/ideaqr/gateway/repository/DecisionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ideaqr.gateway.repository;
-
-import com.ideaqr.gateway.domain.Decision;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DecisionRepository extends JpaRepository<Decision, UUID> {
-}
--- src/main/java/com/ideaqr/gateway/repository/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ideaqr.gateway.repository;
-
-import com.ideaqr.gateway.domain.RequestRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RequestRepository extends JpaRepository<RequestRecord, UUID> {
-}
